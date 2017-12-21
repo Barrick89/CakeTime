8c5f03b9cd7ff4e54193bf4aec49b7fa88bc1066
marku <markus.hauschild.negativ@googlemail.com>
2017-12-21 20:00:15 +0100

    Initial commit

---
+++ app/src/main/java/com/mahausch/caketime/RecipeOverviewActivity.java
@@ -0,0 +1,13 @@
+package com.mahausch.caketime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_overview);
+    }
+}
