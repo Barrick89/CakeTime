0fa0ce4eb4fccd2b54f617c7a89d933ea571cdeb
marku <markus.hauschild.negativ@googlemail.com>
2017-12-23 23:03:37 +0100

    Add CardView dependency

--- app/src/main/java/com/mahausch/caketime/RecipeOverviewActivity.java
+++ app/src/main/java/com/mahausch/caketime/RecipeOverviewActivity.java
@@ -1,7 +1,11 @@
 package com.mahausch.caketime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mahausch.caketime.Utils.JsonUtils;
+
+import java.util.ArrayList;
 
 public class RecipeOverviewActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class RecipeOverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_overview);
+
+        ArrayList<Recipe> list = JsonUtils.getRecipesFromJson(this);
     }
 }
