45fa62048bf18f129ba4154d4a69e66aefdc6ff6
marku <markus.hauschild.negativ@googlemail.com>
2018-01-01 22:47:58 +0100

    Add callback to RecipeDetailActivity and StepAdapter

--- app/src/main/java/com/mahausch/caketime/RecipeDetailFragment.java
+++ app/src/main/java/com/mahausch/caketime/RecipeDetailFragment.java
@@ -1,5 +1,6 @@
 package com.mahausch.caketime;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,13 +14,33 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RecipeDetailFragment extends Fragment {
+public class RecipeDetailFragment extends Fragment implements StepAdapter.StepAdapterOnClickHandler {
 
     @BindView(R.id.recycler_view_detail)
     RecyclerView recycler;
 
+    OnStepClickListener mCallback;
+
     public RecipeDetailFragment() {
         // Required empty public constructor
+    }
+
+    public interface OnStepClickListener {
+        void onStepSelected(RecipeStep step);
+
+        void onIngredientsSelected(ArrayList<Ingredient> ingredients);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mCallback = (OnStepClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnStepClickListener");
+        }
     }
 
 
@@ -33,12 +54,23 @@ public class RecipeDetailFragment extends Fragment {
         Bundle bundle = getArguments();
         Recipe recipe = bundle.getParcelable("recipe");
         ArrayList<RecipeStep> steps = recipe.getRecipeSteps();
+        ArrayList<Ingredient> ingredients = recipe.getIngredients();
 
         recycler.setLayoutManager(new LinearLayoutManager(getContext()));
-        StepAdapter adapter = new StepAdapter();
+        StepAdapter adapter = new StepAdapter(this);
         recycler.setAdapter(adapter);
-        adapter.setStepsList(steps);
+        adapter.setStepsList(steps, ingredients);
 
         return rootView;
+    }
+
+    @Override
+    public void onClickIngredient(ArrayList<Ingredient> ingredients) {
+        mCallback.onIngredientsSelected(ingredients);
+    }
+
+    @Override
+    public void onClickStep(RecipeStep step) {
+        mCallback.onStepSelected(step);
     }
 }
