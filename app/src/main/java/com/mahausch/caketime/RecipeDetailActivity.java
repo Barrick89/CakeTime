e79aa8a8dc0eca129708de06a3551ee2539b74fe
marku <markus.hauschild.negativ@googlemail.com>
2018-01-01 19:43:00 +0100

    Add new RecipeDetailFragment, pass recipe via bundle and add the fragment through the fragmentmanager

--- app/src/main/java/com/mahausch/caketime/RecipeDetailActivity.java
+++ app/src/main/java/com/mahausch/caketime/RecipeDetailActivity.java
@@ -1,13 +1,31 @@
 package com.mahausch.caketime;
 
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class RecipeDetailActivity extends AppCompatActivity {
+
+    private Recipe mRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
+        Bundle intentBundle = getIntent().getExtras();
+        mRecipe = intentBundle.getParcelable("recipe");
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("recipe", mRecipe);
+
+        RecipeDetailFragment detailFragment = new RecipeDetailFragment();
+        detailFragment.setArguments(bundle);
+
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().add(R.id.recipe_detail_fragment, detailFragment).commit();
+
+
     }
 }
