77bf1006a20ce5cb4ecf8cee9159d4a846b8cd8c
marku <markus.hauschild.negativ@googlemail.com>
2018-01-01 21:04:21 +0100

    Delete onClick logic and add recyclerView reference with adding layout manager and adapter

--- app/src/main/java/com/mahausch/caketime/RecipeDetailFragment.java
+++ app/src/main/java/com/mahausch/caketime/RecipeDetailFragment.java
@@ -1,22 +1,22 @@
 package com.mahausch.caketime;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RecipeDetailFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RecipeDetailFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    @BindView(R.id.recycler_view_detail)
+    RecyclerView recycler;
 
     public RecipeDetailFragment() {
         // Required empty public constructor
@@ -27,45 +27,18 @@ public class RecipeDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
-    }
+        final View rootView = inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        ButterKnife.bind(this, rootView);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        Bundle bundle = getArguments();
+        Recipe recipe = bundle.getParcelable("recipe");
+        ArrayList<RecipeStep> steps = recipe.getRecipeSteps();
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        StepAdapter adapter = new StepAdapter();
+        recycler.setAdapter(adapter);
+        adapter.setStepsList(steps);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return rootView;
     }
 }
