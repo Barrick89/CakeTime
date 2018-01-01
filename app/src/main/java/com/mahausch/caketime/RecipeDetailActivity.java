8550a6bda5998bd08f1378b4151835b215d06a9b
marku <markus.hauschild.negativ@googlemail.com>
2018-01-01 22:46:56 +0100

    Add OnIngredientsSelected() to display the ingredients list

--- app/src/main/java/com/mahausch/caketime/RecipeDetailActivity.java
+++ app/src/main/java/com/mahausch/caketime/RecipeDetailActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-public class RecipeDetailActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RecipeDetailActivity extends AppCompatActivity implements RecipeDetailFragment.OnStepClickListener {
 
     private Recipe mRecipe;
 
@@ -27,5 +29,24 @@ public class RecipeDetailActivity extends AppCompatActivity {
         manager.beginTransaction().add(R.id.recipe_detail_fragment, detailFragment).commit();
 
 
+    }
+
+    @Override
+    public void onStepSelected(RecipeStep step) {
+
+    }
+
+    @Override
+    public void onIngredientsSelected(ArrayList<Ingredient> ingredients) {
+
+        IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList("ingredients", ingredients);
+        ingredientsFragment.setArguments(bundle);
+
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.recipe_detail_fragment, ingredientsFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
