bc5eeec3bbe48dbeb2dda87bfdefc557dc3f8d38
marku <markus.hauschild.negativ@googlemail.com>
2018-01-10 18:09:35 +0100

    Add new Espresso test for RecipeOverviewActivity

---
+++ app/src/androidTest/java/com/mahausch/caketime/RecipeOverviewActivityTest.java
@@ -0,0 +1,40 @@
+package com.mahausch.caketime;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.mahausch.caketime.activities.RecipeDetailActivity;
+import com.mahausch.caketime.activities.RecipeOverviewActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeOverviewActivityTest {
+
+
+    @Rule
+    public IntentsTestRule<RecipeOverviewActivity> mActivityTestRule = new IntentsTestRule<>(RecipeOverviewActivity.class);
+
+    //Clicks on a RecyclerView item to check if RecipeDetailActivity is launched.
+
+    @Test
+    public void clickRecyclerViewItem_OpensRecipeDetailActivity() {
+
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+
+        intended(hasComponent(RecipeDetailActivity.class.getName()));
+    }
+
+}
+
